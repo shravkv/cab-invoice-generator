a8a8f2d0e465848855a8a22db4e8dbcbac7ae145
shravkv <sravan.k.vodnala@gmail.com>
2022-07-15 21:57:46 +0530

    Added: Created invoice generator which returns total fare for the journey

--- src/test/java/com/bridgelabz/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/InvoiceGeneratorTest.java
@@ -1,4 +1,26 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class InvoiceGeneratorTest {
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        InvoiceGenerator generator = new InvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double fare = generator.calculateFare(distance, time);
+        Assert.assertEquals(25, fare, 0.0);
+    }
+
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinFare() {
+        InvoiceGenerator generator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = generator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
+
+    }
 }
